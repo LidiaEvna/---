fed5425304cc0aee3b9bcc2464796dd6a47d7081
lida <kurogiri.dabi@gmail.com>
2022-05-19 08:15:08 +0300

    init commit1

--- app/src/main/java/com/example/calculator1/MainActivity.java
+++ app/src/main/java/com/example/calculator1/MainActivity.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText e1, e2;
-    TextView t1;
+    TextView t1,t2;
     int num1, num2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         e1 = (EditText) findViewById(R.id.num1);
         e2 = (EditText) findViewById(R.id.num2);
         t1 = (TextView) findViewById(R.id.t1);
@@ -23,11 +26,7 @@ public class MainActivity extends AppCompatActivity {
     public void doSum(View v) {
         String s1 = e1.getText().toString();
         String s2 = e2.getText().toString();
-        if (s1.equals("") || s2.equals("")) {
-            String result = "Данные не корректные";
-            t1.setText(result);
-            System.out.println(result);
-        } else {
+        try {
             num1 = Integer.parseInt(s1);
             num2 = Integer.parseInt(s2);
             int sum = num1 + num2;
@@ -36,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("key2", num2);
             intent.putExtra("sum", sum);
             startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Данные не корректные", Toast.LENGTH_LONG).show();
+// String result = "Данные не корректные";
+// t1.setText(result);
+// System.out.println(result);
         }
     }
 }
